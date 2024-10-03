ff950bd09340beb81f370c06065e6e3d9c59fe29
Sandhya-scaler <sandhyasingh020348@gmail.com>
2024-10-03 20:02:24 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; 
+// treat all js code with newer version
+
+// alert(3+3): we are using nodejs , not browser
+// datatypes: number=>2^53;bigint;string=>" "or '';boolean=>true/false; undefined=>value is not defined;null=>representation of empty value ; symbol=> unique
+console.log(typeof "hitesh")
+//to see type of value
